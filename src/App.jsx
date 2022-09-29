4fb48c506ac5f3ed27f04e8cb5b82bbddcc2d33b
Berk Bayram <52667956+berkbym@users.noreply.github.com>
2022-09-29 13:18:50 +0100

    create search state

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ export const PropertyContext = React.createContext();
 function App() {
   const [properties, setProperties] = useState([]);
 
+  // use this state to filter properties object by description
   const [filter, setFilter] = useState('');
 
   const filteredProperties = properties.filter(property => {
@@ -32,7 +33,6 @@ function App() {
     <PropertyContext.Provider value={{setFilter}}>
       <div className="container mx-auto my-5">
         <Header />
-
         <div className="grid grid-cols-1 gap-4 mt-5 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
           {!!filteredProperties && filteredProperties.map((property) => <PropertyCard key={property.property_id} property={property} />)}
         </div>
--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -1,10 +1,21 @@
+import React from 'react';
+import { useEffect, useContext, useState } from 'react';
 import { FaSearch } from 'react-icons/fa';
+import { PropertyContext } from './App';
 
 function SearchBox() {
+
+  const {setFilter} = useContext(PropertyContext)
+
+  const [search, setSearch] = useState('')
+
+  useEffect(() => {
+    setFilter(search)
+  }, [search,setFilter])
+
   return (
     <div className="mt-5 relative">
-      <input placeholder="Enter a search term" className="px-5 py-3 border-gray-400 border rounded w-full" />
-
+      <input placeholder="Enter a search term" className="px-5 py-3 border-gray-400 border rounded w-full" value={search} onChange={(e) => setSearch(e.target.value)} />
       <FaSearch className="absolute top-3.5 right-3.5 text-gray-400" size={20} />
     </div>
   );
